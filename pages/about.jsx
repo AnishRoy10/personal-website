0cef1b9b9de81730e0c2b76302149e4863c662b3
Anish Roy <anishroy991@gmail.com>
2024-09-19 23:39:24 -0400

    added basic text and styling to the about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,14 @@
-
+import Button from "@/components/Button";
+import styles from "@/styles/about.module.css";
 
 const about = () => {
-    return <div>
-        about
+    return <div className={styles.about}>
+        <h1> something </h1>
+        <p>
+            Hi, I'm Anish Roy, a computer science student at the University of Toronto.
+            <br /> other stuff
+        </p>
+        <Button path = "/projects">Projects</Button>
     </div>;
 };
 
