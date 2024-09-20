a1286813d06997d406f769824b6b53c6e245cd29
Anish Roy <anishroy991@gmail.com>
2024-09-19 23:15:14 -0400

    added styling to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
 import Button from "@/components/Button";
+import styles from "@/styles/home.module.css"
 // continue at 51 min
 const home = () => {
-  return <div>
+  return <div className={styles.home}>
     <h1>Hi, I'm Anish</h1>
     <p>description</p>
     <Button path="/about">About</Button>
