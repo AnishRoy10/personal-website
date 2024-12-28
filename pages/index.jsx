b30c41d3dc0def874096d0b3fa9c82d700107187
Anish Roy <anishroy991@gmail.com>
2024-12-28 01:35:11 -0500

    fixed import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Button from "@/components/Button";
-import styles from "../styles/home.module.css"
+import styles from "../styles/home.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
